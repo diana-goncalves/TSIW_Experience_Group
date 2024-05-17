98de8c653d46a91ffd5e566fd64caebe9c262613
andresjpg2000 <andresjpg2000@gmail.com>
2024-05-17 19:50:19 +0100

    Login Desktop version

--- js/views/login&register.js
+++ js/views/login&register.js
@@ -1,11 +1,23 @@
  // script para o botão remember me
 
  document.addEventListener('DOMContentLoaded', function () {
-    const rememberMeButton = document.querySelector('#lembrarBTN');
-    const customCheckbox = document.getElementById('checkbox');
+    const rememberMeButton = document.querySelector('#lembrarBTN')
+    const customCheckbox = document.getElementById('checkbox')
 
 rememberMeButton.addEventListener('click', function () {
-    customCheckbox.checked = !customCheckbox.checked;
-    rememberMeButton.classList.toggle('checked', customCheckbox.checked);
-});
-});
+    customCheckbox.checked = !customCheckbox.checked
+    rememberMeButton.classList.toggle('checked', customCheckbox.checked)
+})
+})
+
+// script para mudar entre iniciar sessão e criar conta
+
+document.querySelector("#criarButton").addEventListener('click', function () {
+    document.querySelector("#loginForm").style.display = 'none';
+    document.querySelector("#registerForm").style.display = 'block';
+})
+
+document.querySelector("#loginButton").addEventListener('click', function () {
+    document.querySelector("#loginForm").style.display = 'block';
+    document.querySelector("#registerForm").style.display = 'none';
+})
