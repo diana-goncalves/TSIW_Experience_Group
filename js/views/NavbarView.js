29a00d3037abc1d31ae48126bdbac8decd716ff6
andresjpg2000 <andresjpg2000@gmail.com>
2024-05-19 20:14:20 +0100

    Updates navbar

--- js/views/NavbarView.js
+++ js/views/NavbarView.js
@@ -5,15 +5,24 @@ document.addEventListener("DOMContentLoaded", function() {
     // Seleciona o elemento com a classe 'navbar-collapse'
     var navbarMenu = document.querySelector(".navbar-collapse");
 
+    let navPAI = document.querySelector(".navbar");
+    
+    
     // Adiciona um evento de clique ao elemento 'navbarToggler'
     navbarToggler.addEventListener("click", function() {
         // Verifica se a classe 'show' está presente no elemento 'navbarMenu'
         if (navbarMenu.classList.contains("show")) {
             // Se estiver presente, remove a classe 'show' do elemento 'navbarMenu'
             navbarMenu.classList.remove("show");
+            // Mudar para a altura máxima regular
+            navPAI.style.maxHeight = '60px';
+            
         } else {
             // Se não estiver presente, adiciona a classe 'show' ao elemento 'navbarMenu'
             navbarMenu.classList.add("show");
+            // Mudar a altura máxima para conter todo o conteudo
+            navPAI.style.maxHeight = '300px';
+
         }
     });
 });
