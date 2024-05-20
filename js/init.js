716dbce8d51b4fc51c5b8b9f6d5bf080ebc38393
diana-goncalves <dianafi96@gmail.com>
2024-05-21 00:25:23 +0100

    Try to add gauge animation and Create a footer

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ import * as User from "./models/UserModel.js";
 
 initdata();
 
-function initdata(){
+function initdata() {
     if (!localStorage.items) {
         const items = [
             {
@@ -21,38 +21,37 @@ function initdata(){
                 room: "sala 201"
             },
         ];
-        items.forEach(item =>{
-            invItems.addItem(item.x,item.y,item.name,item.img,item.room)
-        })
+        items.forEach(item => {
+            invItems.addItem(item.x, item.y, item.name, item.img, item.room);
+        });
     }
 
     // USERS
     if (!localStorage.users) {
         const users = [
-        {
-            id: 1,
-            username: "admin",
-            password: "admin"
-        },
-        {
-            id: 2,
-            username: "user1",
-            password: "pass1",
-        },
-        {
-            id: 3,
-            username: "user2",
-            password: "pass2",
-        },
-        {
-            id: 4,
-            username: "user3",
-            password: "pass3",
-        }
+            {
+                id: 1,
+                username: "admin",
+                password: "admin"
+            },
+            {
+                id: 2,
+                username: "user1",
+                password: "pass1",
+            },
+            {
+                id: 3,
+                username: "user2",
+                password: "pass2",
+            },
+            {
+                id: 4,
+                username: "user3",
+                password: "pass3",
+            }
         ];
         users.forEach((user) => {
-        User.add(user.username, user.password);
+            User.add(user.username, user.password);
         });
     }
-
 }
