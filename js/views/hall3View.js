c4b87a19163e369118a469224618102f4c4f815f
M4sterSoul <fabio1999c@gmail.com>
2024-05-24 16:35:03 +0100

    Modificações ER - PARTE 1

---
+++ js/views/hall3View.js
@@ -0,0 +1,78 @@
+$(document).ready(function(e) {
+    $('img[usemap]').rwdImageMaps();
+});
+
+
+//--------------------------------------------------------------------
+// PORTA ESQUERDA
+const hall2LeftArea = document.querySelector("#hall2Left");
+const imgLeft = document.querySelector(".imgLeft");
+
+// PORTA DIREITA
+const hall2RightArea = document.querySelector("#hall2Right");
+const imgRight = document.querySelector(".imgRight"); 
+
+// CORREDOR FRENTE
+const hall2FrontArea = document.querySelector("#hall2tohall3");
+const imgFront = document.querySelector(".imgFront");
+
+// CORREDOR ATRAS
+const hall2BackArea = document.querySelector("#hall2hall1");
+const imgBack = document.querySelector(".imgBack");
+
+
+// PARA APARECER O RELEVO NA PORTA ESQUERDA
+hall2LeftArea.addEventListener("mouseenter", (e)=> {
+    e.preventDefault();
+    imgLeft.style.display = "block";
+});
+
+hall2LeftArea.addEventListener("mouseleave",(e)=>{
+    e.preventDefault();
+    imgLeft.style.display = "none";
+});
+
+// PARA APARECER O RELEVO NA PORTA DIREITA
+hall2RightArea.addEventListener("mouseenter",(e)=>{
+    e.preventDefault();
+    imgRight.style.display="block";
+});
+
+hall2RightArea.addEventListener("mouseleave",(e)=>{
+    e.preventDefault();
+    imgRight.style.display="none";
+});
+
+// PARA APARECER O RELEVO NO CORREDOR FRENTE
+hall2FrontArea.addEventListener("mouseenter",(e)=>{
+    e.preventDefault();
+    imgFront.style.display="block";
+});
+
+hall2FrontArea.addEventListener("mouseleave",(e)=>{
+    e.preventDefault();
+    imgFront.style.display="none";
+});
+
+// PARA APARECER O RELEVO NO CORREDOR EM BAIXO
+hall2BackArea.addEventListener("mouseenter",(e)=>{
+    e.preventDefault();
+    imgBack.style.display="block";
+});
+
+hall2BackArea.addEventListener("mouseleave",(e)=>{
+    e.preventDefault();
+    imgBack.style.display="none";
+});
+
+
+let toastTrigger = document.getElementById('liveToastBtn');
+let toastLiveExample = document.getElementById('liveToast');
+
+const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+toastTrigger.addEventListener('click', () => {
+      toastBootstrap.show()
+});
+        
+
+//--------------------------------------------------------------------
