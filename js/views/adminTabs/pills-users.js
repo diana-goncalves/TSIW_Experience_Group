d1f54867f2927e3f9238b706342d3959f77efbf2
andresjpg2000 <andresjpg2000@gmail.com>
2024-06-02 18:37:02 +0100

    Perfil concluido, gerir users concluido.

--- js/views/adminTabs/pills-users.js
+++ js/views/adminTabs/pills-users.js
@@ -1,8 +1,5 @@
 import * as User from "../../models/UserModel.js"
 
-// Iniciar
-renderTableUsers(User.getUsers());
-
 function renderTableUsers(users = []) {
     
     const tabelaUsers = document.querySelector("#table-Users");
@@ -72,6 +69,8 @@ function renderTableUsers(users = []) {
         })
     }
 
+    tableHeaders();
+
 }
 
 // Procurar user pelo nome automatico
@@ -81,21 +80,121 @@ filterInputUsers.addEventListener("input", () => {
     renderTableUsers(User.getUsers(filterInputUsers.value));
 })
 
-// Clicar no botão organizar
-let isSorted = false;
-const orderButtonUsers = document.querySelector("#btnOrderUser");
 
-orderButtonUsers.addEventListener("click", () => {
+// Organizar colunas da tabela
+let headersEvents = false;
+function tableHeaders() {
     
-    if(isSorted) {
-        // Caso já tenham clicado para organizar, tirar o sort
-        renderTableUsers(User.getUsers(filterInputUsers.value));
-    } else {
-        // Organizar a lista de users filtrados, se não houver filtros organiza APENAS a tabela
-        renderTableUsers(User.sortUsers(User.getUsers(filterInputUsers.value)));
+    // Evitar colocar event listeners 2x
+    if (headersEvents) {
+        return;
+    } 
+
+    headersEvents = true;
+    
+    // Clicar nos headers da tabela para organizar linhas
+    let isSorted = false;
+    const tHeader = document.querySelector("#tHeadUsers");
+    const tHeaders = tHeader.querySelectorAll("th");
+    
+    tHeaders.forEach(header => {
+        const headerIndex = parseInt(header.getAttribute("id"));
+
+        // Excluir coluna com botão para gerir users e ID de user
+        if (headerIndex != 7 && headerIndex != 0) {
+            header.style.cursor = "pointer"; // Mudar cursor para indicar elemento "clicavel"
+        
+            header.addEventListener("click", () => {
+                isSorted = !isSorted;
+                sortTable(headerIndex, isSorted);
+            })
+        }
+
+    });    
+}
+
+let original = null;
+function sortTable(colIndex, isSorted) {
+
+    let users = User.getUsers();
+
+    if ( original === null) {
+        // Criar uma cópia dos users antes de organizar
+        original = users.slice();
+    }
+
+    // Voltar à ordem original
+    if (!isSorted) {
+        renderTableUsers(original);
+        return;
+    }
+
+   users.sort((a,b) => {
+    let aContent = null;
+    let bContent = null;
+
+    switch (colIndex) {
+        case 1:
+            // Coluna username
+            aContent = a.username;
+            bContent = b.username;
+
+            break;
+        case 2:
+            // Coluna primeiro nome
+            aContent = a.firstName ? a.firstName : "N/A";
+            bContent = b.firstName ? b.firstName : "N/A";
+            
+            break;
+        case 3:
+            // Coluna apelido
+            aContent = a.lastName ? a.lastName : "N/A";
+            bContent = b.lastName ? b.lastName : "N/A";
+            
+            break;
+        case 4:
+            // Coluna localidade
+            aContent = a.location ? a.location : "N/A";
+            bContent = b.location ? a.location : "N/A";
+            
+            break;
+        case 5:
+            // Coluna género
+            aContent = a.gender ? a.gender : "N/A";
+            bContent = b.gender ? a.gender : "N/A";
+            
+            break;
+        case 6:
+            // Coluna data de nascimento, foi necessario converter para string para conseguir comparar com as linhas que não têm data
+            aContent = a.birthdate ? new Date(a.birthdate).toISOString() : "N/A";
+            bContent = b.birthdate ? new Date(b.birthdate).toISOString() : "N/A";
+            
+            break;
+        default:
+            return 0;
     }
     
-    isSorted = !isSorted;
-})
+    if (typeof aContent === "string" || typeof bContent === "string") {
+        aContent = aContent.toLowerCase();
+        bContent = bContent.toLowerCase();
+    }
+    
+    if (aContent === bContent) {
+        return 0;
+    }
+
+    if (aContent > bContent) {
+        return 1;
+    } else {
+        return -1;
+    }
+
+   })
+
+   renderTableUsers(users);
+
+}
 
 
+// Iniciar
+renderTableUsers(User.getUsers());
