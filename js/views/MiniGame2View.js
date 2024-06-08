629b672a608e2ace30dca2bd74ac2a1e631323d6
M4sterSoul <fabio1999c@gmail.com>
2024-06-08 18:37:03 +0100

    Adição de imagens para minigame

--- js/views/MiniGame2View.js
+++ js/views/MiniGame2View.js
@@ -7,6 +7,10 @@ let options = [
         certo: `../../media/img/ER-assets/mini-game1/html1.png`,
         opt: [`../../media/img/ER-assets/mini-game1/html1.png`,`../../media/img/ER-assets/mini-game1/html2.png`,`../../media/img/ER-assets/mini-game1/html3.png`,`../../media/img/ER-assets/mini-game1/html4.png`]
     },
+    {
+        certo: `../../media/img/ER-assets/mini-game1/vscode1.png`,
+        opt: [`../../media/img/ER-assets/mini-game1/vscode1.png`,`../../media/img/ER-assets/mini-game1/vscode2.png`,`../../media/img/ER-assets/mini-game1/vscode3.png`,`../../media/img/ER-assets/mini-game1/vscode4.png`]
+    },
 ];
 
 let Exercise = [];
@@ -16,6 +20,7 @@ let modalGame =  new bootstrap.Modal(document.querySelector("#miniGame2"))
 
 let startGame = () =>{  
     let index = Math.floor(Math.random() * options.length);//nao pode ter o mesmo index
+    console.log(index);
     if (Exercise.some(element => element.certo == options[index].certo)) {
         startGame();
     }else{
@@ -24,16 +29,18 @@ let startGame = () =>{
             let img = document.createElement("img");
             img.classList.add("w-50", "h-50", "logo");
             img.src = element;
-            // if (condition) {
+            if (element == options[index].certo) {
                 
-            // }
+            }else{
+
+            }
             document.querySelector(`.card${i+1}`).appendChild(img)
         });
     }
 }
 
 let resetGame = ()=>{
-    for (let i = 1; i <= 3; i++) {
+    for (let i = 1; i <= 4; i++) {
         document.querySelector(`.card${i}`).innerHTML = "";
     }
     Exercise = [];
