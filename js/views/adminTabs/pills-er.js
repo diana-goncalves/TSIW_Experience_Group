9316ab33590b09a10cd973400f7c9ce4feb8497b
andresjpg2000 <andresjpg2000@gmail.com>
2024-06-18 11:38:58 +0100

    Adicionar colecionaveis

---
+++ js/views/adminTabs/pills-er.js
@@ -0,0 +1,198 @@
+import * as Collectible from "../../models/collectiblesModel.js";
+
+// Variaveis que gerem páginas das tabelas
+const tableRows = 5;
+let currentPage = 1;
+let totalPages = 1;
+
+// Form submit para editar e adicionar colecionaveis
+function submitForm() {
+    const form = document.querySelector("#formCol");
+    const fileInput = document.querySelector("#fileInputColecionaveis");
+    const previewImg = document.querySelector("#imgPreviewCol");
+    
+    form.addEventListener("submit",(event) => {
+        event.preventDefault();
+
+        const imgData = fileInput.files[0];
+        
+        // Guardar valores dos inputs
+        const colData = {
+            nome: document.querySelector("#formNomeCol").value,
+            sala: document.querySelector("#formSalaCol").value,
+            largura: document.querySelector("#formLarguraCol").value,
+            altura: document.querySelector("#formAlturaCol").value,
+            x: document.querySelector("#formPosxCol").value,
+            y: document.querySelector("#formPosyCol").value,
+            photo: previewImg.src
+        }
+        
+        // Criar instancia do file reader que converte a imagem para string
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function () {
+            
+            resizeImage(reader.result, (resizedImg) => {
+                colData.photo = resizedImg;
+
+                submitCol(colData);
+            }) 
+            
+        })
+
+        reader.readAsDataURL(imgData);
+        
+    });
+
+    fileInput.addEventListener("change", (event) => {
+        const imgData = event.target.files[0];
+
+        if (imgData) {
+            // Adicionar pre-visualização da imagem quando há uma imagem selecionada
+            const reader = new FileReader();
+
+            reader.addEventListener("load", function () {
+                
+                resizeImage(reader.result, (resizedImg) => {
+                    previewImg.src = resizedImg;
+                    previewImg.style.display = "block";
+                }) 
+               
+            })
+
+            reader.readAsDataURL(imgData);
+
+        } else {
+            
+            previewImg.src = "";
+            previewImg.style.display = "none";
+        }
+
+    })
+    
+}
+
+function submitCol(colData) {
+    try {
+        // Adicionar colecionavel
+        Collectible.addCollectible(colData.largura, colData.altura, colData.x, colData.y, colData.nome, colData.photo, colData.sala);
+        customToast("Colecionável adicionado com sucesso!")
+
+        // limpar inputs
+        document.querySelector("#formCol").reset();
+        // atualizar tabela
+        // renderTableProjects(Project.getProjects(), currentPage);
+
+    } catch (error) {
+        customToast(error.message);
+    } 
+
+}
+
+function initCancelButton() {
+    
+    const form = document.querySelector("#formCol");
+    const cancelEditCol = document.querySelector("#cancelEditCol");
+    const img = document.querySelector("#imgPreviewCol");
+    
+    
+    form.addEventListener("input", () => {
+        
+        const inputs = form.querySelectorAll("input[type='text']");
+        let verificarValores = false;
+
+        inputs.forEach(input => {
+            if(input.value.trim() !== "") {
+                verificarValores = true;
+            }
+        })
+          
+        // adicionar/remover botão para dar reset ao form. Se os inputs tiverem algum valor, adiciona o botão, se estiverem vazios, remove.
+        cancelEditCol.style.display = verificarValores ? "block" : "none";
+
+    })
+
+    // remover botão depois de dar reset
+    cancelEditCol.addEventListener("click", () => {
+        cancelEditCol.style.display = "none";
+        // remover image preview
+        img.style.display = "none";
+    })
+
+    form.addEventListener("submit", () => {
+        cancelEditCol.style.display = "none";
+        img.style.display = "none";
+    })
+
+}
+
+// Form submit para editar e adicionar colecionaveis
+
+function customToast(message) {
+    
+    document.querySelector("#adminToast").textContent = message;
+
+    var toast = new bootstrap.Toast(document.querySelector("#alertToast"));
+
+    toast.show();
+}
+
+function resizeImage(imgURL, callback) {
+    const img = new Image();
+
+    // Alterar tamanho apenas quando imagem acabar de carregar
+    img.addEventListener("load", () => {
+        
+        const maxWidth = 800;       // Ajustar estes valores para aumentar/diminuir tamanho das imagens
+        const maxHeight = 600;
+
+        let width = img.width;
+        let height = img.height;
+
+        //  Dar resize da imagem caso seja necessario, sem perder aspect ratio
+        if (width > maxWidth || height > maxHeight) {
+    
+            const aspectRatio = width/height;
+
+            if (width > maxWidth) {
+                
+                width = maxWidth;
+
+                height = width / aspectRatio;
+
+            } else {
+                
+                height = maxHeight;
+
+                width = height * aspectRatio;
+            }
+
+        }
+
+        const canvas = document.createElement("canvas");
+
+        const content = canvas.getContext("2d");
+
+        canvas.width = width;
+        canvas.height = height;
+
+        content.drawImage(img, 0, 0, width, height);
+
+        const newUrl = canvas.toDataURL("image/jpeg");
+
+        callback(newUrl);
+
+    })
+
+    // Adicionar src ao novo elemento img criado
+    img.src = imgURL;
+
+}
+
+// INICIAR
+Collectible.init()
+
+submitForm();
+
+initCancelButton();
+
--- js/views/hall3View.js
+++ js/views/hall3View.js
@@ -10,70 +10,6 @@ $(document).ready(function(e) {
 
 let noKey =  new bootstrap.Modal(document.querySelector("#noKeyModal"));
 
-// Cofre e código
-let cofreModal = new bootstrap.Modal(document.querySelector("#cofreModal"));
-
-let code1 = parseInt(document.querySelector("#code1").textContent);
-document.querySelector("#code1UP").addEventListener("click", () => {
-    code1===10 ? code1 : code1++;
-    document.querySelector("#code1").textContent = `${code1}`;
-})
-document.querySelector("#code1DOWN").addEventListener("click", () => {
-    code1===0 ? code1 : code1--;
-    document.querySelector("#code1").textContent = `${code1}`;
-})
-
-let code2 = parseInt(document.querySelector("#code2").textContent);
-document.querySelector("#code2UP").addEventListener("click", () => {
-    code2===10 ? code2 : code2++;
-    document.querySelector("#code2").textContent = `${code2}`;
-})
-document.querySelector("#code2DOWN").addEventListener("click", () => {
-    code2===0 ? code2 : code2--;
-    document.querySelector("#code2").textContent = `${code2}`;
-})
-
-let code3 = parseInt(document.querySelector("#code3").textContent);
-document.querySelector("#code3UP").addEventListener("click", () => {
-    code3===10 ? code3 : code3++;
-    document.querySelector("#code3").textContent = `${code3}`;
-})
-document.querySelector("#code3DOWN").addEventListener("click", () => {
-    code3===0 ? code3 : code3--;
-    document.querySelector("#code3").textContent = `${code3}`;
-})
-
-let code4 = parseInt(document.querySelector("#code4").textContent);
-document.querySelector("#code4UP").addEventListener("click", () => {
-    code4===10 ? code4 : code4++;
-    document.querySelector("#code4").textContent = `${code4}`;
-})
-document.querySelector("#code4DOWN").addEventListener("click", () => {
-    code4===0 ? code4 : code4--;
-    document.querySelector("#code4").textContent = `${code4}`;
-})
-
-const abrirCofre = document.querySelector("#abrirCofre");
-const bodyCofreModal = document.querySelector("#bodyCofreModal");
-const heroIMG = document.querySelector("#heroHall3");
-const chaveTSIW = document.querySelector("#chaveTSIW");
-
-// Clicar no botão para abrir cofre. Comparar codigo inserido com codigo na session storage.
-abrirCofre.addEventListener("click", () => {
-    const codigoInserido = `${code1}` + `${code2}` +`${code3}` + `${code4}`;
-    
-    const jogo = JSON.parse(sessionStorage.getItem("gameStatus"));
-    const codigoJogo = `${jogo.code[0]}` + `${jogo.code[1]}` +`${jogo.code[2]}` + `${jogo.code[3]}`;
-
-    if (codigoInserido === codigoJogo) {
-        cofreModal.hide();
-        heroIMG.src = "../../media/img/ER-assets/hall3CofreAberto.png";
-        chaveTSIW.style.display = "block";
-    }
-})
-
-// Cofre e código
-
 function hall3View() {
     GameStateView("hall 3");
     // Mete os items na sala
@@ -99,7 +35,10 @@ function hall3View() {
     const portaFinal = document.querySelector("#portaFinal");
 
     // CHAVE TSIW
-    
+    const chaveFinal = document.querySelector("#chaveTSIW");
+
+    // IMAGEM PRINCIPAL HALL 3
+    const heroIMG = document.querySelector("#heroHall3");
 
     // PARA APARECER O RELEVO NA PORTA ESQUERDA
     hall3LeftArea.addEventListener("mouseenter", (e)=> {
@@ -152,8 +91,7 @@ function hall3View() {
 
     portaFinal.addEventListener("click", (e)=>{
         if (checkItemInventory("chaveTSIW")) {
-            // Trocar por modal de vitoria e trocar hero img por hall3 sem a porta final
-            alert("vitoria")
+            heroIMG.src = `../../media/img/ER-assets/hall3FIM.jpg`;
         } else {
             noKey.show();
         }
@@ -164,13 +102,74 @@ function hall3View() {
         cofreModal.show();
     })
 
-    chaveTSIW.addEventListener("click", (e) => {
+    chaveFinal.addEventListener("click", (e) => {
         e.preventDefault();
         // chamar função que coloca chave no inventario
-        chaveTSIW.style.display = "none";
+        chaveFinal.style.display = "none";
     })
 
 }
 hall3View();
 
 //--------------------------------------------------------------------
+
+// Cofre e código
+let cofreModal = new bootstrap.Modal(document.querySelector("#cofreModal"));
+
+let code1 = parseInt(document.querySelector("#code1").textContent);
+document.querySelector("#code1UP").addEventListener("click", () => {
+    code1===10 ? code1 : code1++;
+    document.querySelector("#code1").textContent = `${code1}`;
+})
+document.querySelector("#code1DOWN").addEventListener("click", () => {
+    code1===0 ? code1 : code1--;
+    document.querySelector("#code1").textContent = `${code1}`;
+})
+
+let code2 = parseInt(document.querySelector("#code2").textContent);
+document.querySelector("#code2UP").addEventListener("click", () => {
+    code2===10 ? code2 : code2++;
+    document.querySelector("#code2").textContent = `${code2}`;
+})
+document.querySelector("#code2DOWN").addEventListener("click", () => {
+    code2===0 ? code2 : code2--;
+    document.querySelector("#code2").textContent = `${code2}`;
+})
+
+let code3 = parseInt(document.querySelector("#code3").textContent);
+document.querySelector("#code3UP").addEventListener("click", () => {
+    code3===10 ? code3 : code3++;
+    document.querySelector("#code3").textContent = `${code3}`;
+})
+document.querySelector("#code3DOWN").addEventListener("click", () => {
+    code3===0 ? code3 : code3--;
+    document.querySelector("#code3").textContent = `${code3}`;
+})
+
+let code4 = parseInt(document.querySelector("#code4").textContent);
+document.querySelector("#code4UP").addEventListener("click", () => {
+    code4===10 ? code4 : code4++;
+    document.querySelector("#code4").textContent = `${code4}`;
+})
+document.querySelector("#code4DOWN").addEventListener("click", () => {
+    code4===0 ? code4 : code4--;
+    document.querySelector("#code4").textContent = `${code4}`;
+})
+
+const abrirCofre = document.querySelector("#abrirCofre");
+const heroIMG = document.querySelector("#heroHall3");
+const chaveTSIW = document.querySelector("#chaveTSIW");
+
+// Clicar no botão para abrir cofre. Comparar codigo inserido com codigo na session storage.
+abrirCofre.addEventListener("click", () => {
+    const codigoInserido = `${code1}` + `${code2}` +`${code3}` + `${code4}`;
+    
+    const jogo = JSON.parse(sessionStorage.getItem("gameStatus"));
+    const codigoJogo = `${jogo.code[0]}` + `${jogo.code[1]}` +`${jogo.code[2]}` + `${jogo.code[3]}`;
+
+    if (codigoInserido === codigoJogo) {
+        cofreModal.hide();
+        heroIMG.src = "../../media/img/ER-assets/hall3CofreAberto.png";
+        chaveTSIW.style.display = "block";
+    }
+})
