d2250bcf3933e013eb6a1079fae5d0ee0a3ec4d5
M4sterSoul <fabio1999c@gmail.com>
2024-06-20 20:11:51 +0100

    modificações sala 207

--- js/views/sala207View.js
+++ js/views/sala207View.js
@@ -19,6 +19,10 @@ function sala207View() {
     const salaPortaArea = document.querySelector("#sala207Porta");
     const imgSaida = document.querySelector(".imgSaida");
     const imgComputador = document.querySelector(".imgComputador");
+    const salaPcArea = document.querySelector("#sala207Pc1");
+    const salaPc2Area = document.querySelector("#sala207Pc2");
+    const imgLeft = document.querySelector(".imgLeft");
+    const imgRight = document.querySelector(".imgRight");
 
     // listener para criar o efeito de hover
     sala207QuadroArea.addEventListener("mouseenter", (e)=> {
@@ -39,6 +43,26 @@ function sala207View() {
     salaPortaArea.addEventListener("mouseleave",(e)=>{
         e.preventDefault();
         imgSaida.style.display="none"
+    });
+
+    salaPcArea.addEventListener("mouseenter",(e)=>{
+        e.preventDefault();
+        imgLeft.style.display="block"
+    });
+
+    salaPcArea.addEventListener("mouseleave",(e)=>{
+        e.preventDefault();
+        imgLeft.style.display="none"
+    });
+    
+    salaPc2Area.addEventListener("mouseenter",(e)=>{
+        e.preventDefault();
+        imgRight.style.display="block"
+    });
+
+    salaPc2Area.addEventListener("mouseleave",(e)=>{
+        e.preventDefault();
+        imgRight.style.display="none"
     });
 }
 
